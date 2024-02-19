e43ba4a3a8915625db174a1b1ba99303242a234a
elsalumacang <e.lumacang.61622.dc@umindanao.edu.ph>
2024-02-19 17:57:36 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,25 +1,33 @@
- package LumacangActivity2;
+import java.util.Scanner;
+public class Activity1 {
+    public static void main (String [] args) {
 
- 
-public class Main {
+    java.util.Scanner data = new Scanner (System.in);
+    int age;
+    String firstName;
+    String middleName;
+    String lastName;
+    String birthday;
+    String gender;
 
-  
-    public static void main(String[] args) {
-      
-        int[] numbers = {8, 22, 23, 30, 143};
-        
-        System.out.println("Arrays: " + numbers [0]);
-        System.out.println("Arrays: " + numbers [1]);
-        System.out.println("Arrays: " + numbers [2]);
-        System.out.println("Arrays: " + numbers [3]);
-        System.out.println("Arrays: " + numbers [4]);
-        
-        int summation = numbers [0] + numbers [1] + numbers [2] + numbers [3] + numbers [4];
-        System.out.println("Sum of all arrays: " + summation);
-        
-        int maximum = Math.max(numbers [0], Math.max(numbers [1], Math.max(numbers [2], Math.max(numbers [3], numbers [4]))));
-        System.out.println("Maximum value: " + maximum);
-       
-                  }
-    
+    System.out.println("First Name: ");
+    firstName = data.nextLine();
+
+    System.out.println("Middle Name: ");
+    middleName = data.nextLine();
+
+    System.out.println("Last Name: ");
+    lastName = data.nextLine();
+
+    System.out.println("Birthday: ");
+    birthday = data.nextLine();
+
+    System.out.println("Gender: ");
+    gender = data.nextLine();
+
+    System.out.println("\n" + "First Name: " + firstName + "\n" + "Middle Name: " + middleName + "\n" + 
+                    "Last Name: " + lastName + "\n" + "Birthday:  " + birthday + "\n" + "Gender: " + gender);
+
+
+    }
 }
